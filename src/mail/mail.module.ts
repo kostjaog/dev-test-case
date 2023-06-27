7fdaffe2453ae083e7bfd3884bc0024e40b2bb49
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2023-06-27 22:19:17 +0300

    add salt rounds

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,14 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { join } from 'path';
 
 @Module({
   imports: [
     MailerModule.forRoot({
       // transport: 'smtps://user@example.com:topsecret@smtp.example.com',
-      // or
       transport: {
         host: 'smtp.example.com',
         secure: false,
